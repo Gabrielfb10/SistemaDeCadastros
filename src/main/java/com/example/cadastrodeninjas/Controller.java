47d95fc61412504916b9d8f0538221e710973eef
Acirio Gabriel <agabrielfb11@gmail.com>
2026-07-19 02:48:33 -0300

    Criando controller e adicionando tomcat

---
+++ src/main/java/com/example/cadastrodeninjas/Controller.java
@@ -0,0 +1,15 @@
+package com.example.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa é a mensagem de boas vindas.";
+    }
+}
